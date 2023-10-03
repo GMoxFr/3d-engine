810c20b77b2841fc131e148d2a73032bfe535902
Tristan Nobre <nobretristan@gmail.com>
2023-10-03 14:32:37 +0200

    install dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Function to detect the OS
+detect_os() {
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo ${machine}
+}
+
+# Function to install dependencies on Linux
+install_linux() {
+    echo "Installing dependencies for Linux..."
+    
+    sudo apt-get update
+    sudo apt-get install -y build-essential libsfml-dev make
+}
+
+# Function to install dependencies on macOS
+install_mac() {
+    echo "Installing dependencies for macOS..."
+    
+    # Assuming you have Homebrew installed
+    brew install sfml make openal-soft
+}
+
+OS=$(detect_os)
+
+if [ "$OS" == "Linux" ]; then
+    install_linux
+elif [ "$OS" == "Mac" ]; then
+    install_mac
+else
+    echo "Unsupported OS"
+    exit 1
+fi
